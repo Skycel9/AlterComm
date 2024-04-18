16d3516fc153e7048788aebfa2030db7fe5174f1
skycel <cece.burst@gmail.com>
2024-04-18 17:36:38 +0200

    Correction routing sous-dossier

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ $request = $_SERVER['REQUEST_URI'];
 $files = scandir("views");
 $page_name = "";
 
-if ($request === "/") {
+$base_url = "/ACAD_MMI/";
+
+if ($request === $base_url) {
 	require __DIR__ . '/views/index.php';
 }
 
@@ -12,7 +14,7 @@ foreach ($files as $file) {
 	if ($file !== "." && $file !== "..") {
 		$file = str_replace(".php", "", $file);
 		
-		if ($request === "/" . $file) {
+		if ($request === $base_url . $file) {
 			require __DIR__ . "/views/$file.php";
 		}
 	}
