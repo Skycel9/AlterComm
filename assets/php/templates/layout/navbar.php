067f4ac82f31602331854f34b45405cec9f76ac2
sd <sashadaza0902@gmail.com>
2024-04-18 17:38:52 +0200

    comit test

--- assets/php/templates/layout/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="about.php">À propos</a></li>
-        <li><a href="contact.php">Contact</a></li>
-    </ul>
-</nav>
--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
 
+$base_url = "/";
+
 $files = scandir("views");
 $page_name = "";
 
-if ($request === "/") {
-	require __DIR__ . '/views/index.php';
+if ($request === $base_url) {
+	require __DIR__ . "$base_url/views/index.php";
 }
 
 foreach ($files as $file) {
 	if ($file !== "." && $file !== "..") {
 		$file = str_replace(".php", "", $file);
 		
-		if ($request === "/" . $file) {
-			require __DIR__ . "/views/$file.php";
+		if ($request === $base_url . "/views/" . $file) {
+			require __DIR__ . "/$base_url"."/views/$file.php";
 		}
 	}
 }
