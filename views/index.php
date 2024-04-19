054e4e5163bf09c37eb76ec8c547c548cc1f017f
sd <sashadaza0902@gmail.com>
2024-04-19 13:56:07 +0200

    bcp

--- views/index.php
+++ views/index.php
@@ -1,7 +1,22 @@
 <?php
 @include "assets/php/templates/layout/head.php";
 @include "assets/php/templates/layout/navbar.php";
- 
+?>
 
+<header class="hero">
+	<hgroup>
+		<h1 class="title">Alter&Comm</h1>
+		<p class="tagline">De l’ombre à la lumière, votre projet révélé !</p>
+		<p>Agence de communication</p>
+	</hgroup>
+</header>
 
+<main class="container">
+	<section>
+		<h2>C'est quoi <span class="text-primary">Alter&Comm ?</h2>
+		<p>Notre agence de communication Alter&Comm. est spécialisée dans la publicité. Notre équipe est composé d’un large panel de compétences qui mêlent développement web, stratégie de communication, graphisme, production audiovisuelle, etc. Chacun de nos travaux sont le fruits de la collaboration de toutes ces compétences, assurant ainsi un rendu à la hauteur des attentes de nos clients. Nous sommes aptes à travailler en laissant notre créativité s’exprimer, ou sous restrictions. En outre, la jeunesse fleurissante de notre équipe permet de pouvoir adapter chacun de nos projets dans le dynamisme de l’actualité. Cette agence a été créée en 2021, à l’occasion d’une collaboration entre des étudiants de l’IUT Nancy Charlemagne et l’IUT de Saint Die. Grâce à cette collaboration, nous proposons à nos clients de bénéficier de la philosophie de notre agence : De l’ombre à la lumière, votre projet révélé.</p>
+	</section>
+</main>
+
+<?php
 @include "assets/php/templates/layout/footer.php";
