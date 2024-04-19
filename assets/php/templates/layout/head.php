bc776259526bf3978fbe2603d6c43efba50db70d
skycel <cece.burst@gmail.com>
2024-04-19 09:34:46 +0200

    Autoload + début page index

--- assets/php/templates/layout/head.php
+++ assets/php/templates/layout/head.php
@@ -5,6 +5,24 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title></title>
+	<title><?= $page_name ?></title>
+
+	<?php
+	$componentsStyle = scandir(HOME_URL ."/assets/css/components");
+	foreach ($componentsStyle as $file) {
+		if ($file !== ".." && $file !== ".") {
+			echo "<link rel='stylesheet' type='text/css' href='".$base_url."assets/css/components/$file'>";
+		}
+	}
+	
+	$layoutsStyle = scandir(HOME_URL ."/assets/css/components");
+	foreach ($layoutsStyle as $file) {
+		if ($file !== ".." && $file !== ".") {
+			echo "<link rel='stylesheet' type='text/css' href='".$base_url."assets/css/layout/$file'>";
+		}
+	}
+	?>
+	
+	<link rel="stylesheet" href="<?= $base_url ?>assets/css/pages/style.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
 
+const HOME_URL = __DIR__;
+
 $files = scandir("views");
 $page_name = "";
 
 $base_url = "/ACAD_MMI/";
 
 if ($request === $base_url) {
+	$page_name = "Alter&comm";
 	require __DIR__ . '/views/index.php';
 }
 
@@ -15,6 +18,7 @@ foreach ($files as $file) {
 		$file = str_replace(".php", "", $file);
 		
 		if ($request === $base_url . $file) {
+			$page_name = "Alter&comm - " . ucfirst(str_replace("-", " ", $file));
 			require __DIR__ . "/views/$file.php";
 		}
 	}
