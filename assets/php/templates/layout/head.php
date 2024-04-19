298afb01bfb377bcb63428704040a7e0af3c3029
skycel <cece.burst@gmail.com>
2024-04-19 10:06:17 +0200

    Ajout de la favicon

--- assets/php/templates/layout/head.php
+++ assets/php/templates/layout/head.php
@@ -5,7 +5,14 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="icon" type="image/png" href="/ACAD_MMI/assets/media/favicon.png">
+	
 	<title><?= $page_name ?></title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 	<?php
 	$componentsStyle = scandir(HOME_URL ."/assets/css/components");
